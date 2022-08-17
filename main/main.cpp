ce9244ce04aefc110c7089fc4820e1247af7e700
karthiksb0307 <karthiksbhandge@gmail.com>
2022-08-17 14:17:32 +0530

    template

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,33 @@
+/* Hello World Example
+ 
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+ 
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 #include <stdio.h>
-
-// Enabling C++ compile
-extern "C" { void app_main(); }
-
-void app_main(void)
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "systeminfo.h"
+ 
+extern "C" {
+   void app_main();
+}
+ 
+void app_main()
 {
-
+    printf("Hello world!\n");
+ 
+    /* Print chip information */
+    esp_systeminfo info;
+    info.print();
+ 
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
 }
---
+++ main/systeminfo.cpp
@@ -0,0 +1,13 @@
+#include "systeminfo.h" 
+ 
+void esp_systeminfo::print() 
+{ 
+   esp_chip_info(&chip_info); 
+   printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ", chip_info.cores,
+   (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+   (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+ 
+   printf("silicon revision %d, ", chip_info.revision);     
+   printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+   (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external"); 
+}
